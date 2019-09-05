8f0a61a0d10e93a7119d860a0008d2edd7c3a66e
ivanred6 <ivanfpanton@gmail.com>
2019-09-05 19:29:14 +0100

    Create RoadsAndLibraries.cs
    Initial challenge code. 
    TODO Add correct solution code

---
+++ RoadsAndLibraries.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the roadsAndLibraries function below.
+    static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities) {
+
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            string[] nmC_libC_road = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nmC_libC_road[0]);
+
+            int m = Convert.ToInt32(nmC_libC_road[1]);
+
+            int c_lib = Convert.ToInt32(nmC_libC_road[2]);
+
+            int c_road = Convert.ToInt32(nmC_libC_road[3]);
+
+            int[][] cities = new int[m][];
+
+            for (int i = 0; i < m; i++) {
+                cities[i] = Array.ConvertAll(Console.ReadLine().Split(' '), citiesTemp => Convert.ToInt32(citiesTemp));
+            }
+
+            long result = roadsAndLibraries(n, c_lib, c_road, cities);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
